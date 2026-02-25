4da4b4af53bf739065b8e18912be6ac72f7d00b5
navestelar <ana.carol.huntemann@gmail.com>
2026-02-24 23:25:45 -0300

    refact: env service

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,15 +1,14 @@
 import { NestFactory } from '@nestjs/core'
-import { HttpModule } from './http/http.module'
-import { ConfigService } from '@nestjs/config'
-import { Env } from './env'
+import { AppModule } from './app.module'
+import { EnvService } from './env/env.service'
 
 async function bootstrap() {
-  const app = await NestFactory.create(HttpModule, {
+  const app = await NestFactory.create(AppModule, {
     // logger: false,
   })
 
-  const configService = app.get<ConfigService<Env, true>>(ConfigService)
-  const port = configService.get('PORT', { infer: true })
+  const configService = app.get(EnvService)
+  const port = configService.get('PORT')
 
   await app.listen(port)
 }
