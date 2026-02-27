ae7192a633e25bf66339a40662ce2d4140395a01
navestelar <ana.carol.huntemann@gmail.com>
2026-02-26 22:22:00 -0300

    feat: controllers

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,29 +1,32 @@
 import { Module } from '@nestjs/common'
-import { CreateAccountController } from '@/infra/http/controllers/create-account.controller'
-import { AuthenticateController } from '@/infra/http/controllers/authenticate.controller'
-import { CreateQuestionController } from '@/infra/http/controllers/create-question.controller'
-import { FetchRecentQuestionsController } from '@/infra/http/controllers/fetch-recent-questions.controller'
-import { AuthModule } from '@/infra/auth/auth.module'
+import { AuthenticateController } from './controllers/authenticate.controller'
+import { CreateAccountController } from './controllers/create-account.controller'
+import { CreateQuestionController } from './controllers/create-question.controller'
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
 import { DatabaseModule } from '../database/database.module'
 import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
 import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student'
 import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student'
 import { CryptographyModule } from '../cryptography/cryptography.module'
+import { GetQuestionBySlugController } from './controllers/get-question-by-slug.controller'
+import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug'
 
 @Module({
-  imports: [AuthModule, DatabaseModule, CryptographyModule],
+  imports: [DatabaseModule, CryptographyModule],
   controllers: [
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
     FetchRecentQuestionsController,
+    GetQuestionBySlugController,
   ],
   providers: [
     CreateQuestionUseCase,
     FetchRecentQuestionsUseCase,
     RegisterStudentUseCase,
     AuthenticateStudentUseCase,
+    GetQuestionBySlugUseCase,
   ],
 })
 export class HttpModule {}
